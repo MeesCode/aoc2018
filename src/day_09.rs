31384b75230656c1be57ff7245a32db39d03db79
MeesCode <m.brinkhuis@student.tudelft.nl>
2021-11-03 16:13:13 +0100

    finished day 9

--- src/day_09.rs
+++ src/day_09.rs
@@ -1,4 +1,5 @@
 use regex::Regex;
+use std::collections::VecDeque;
 
 pub fn run(){
     let input = include_str!("../data/day_09.txt").trim();
@@ -9,11 +10,11 @@ pub fn run(){
     let limit = cap[2].parse::<usize>().unwrap();
 
     
-    println!("Day 1");
+    println!("Day 9");
     let a = part_a(players, limit);
     println!("Part A result: {}", a);
-    // let b = part_a(players, limit * 100);
-    // println!("Part B result: {}", b);
+    let b = part_a(players, limit * 100);
+    println!("Part B result: {}", b);
 }
 
 fn get_index(index: i32, length: usize) -> usize{
@@ -28,31 +29,26 @@ fn get_index(index: i32, length: usize) -> usize{
     index as usize
 }
 
-fn part_a(players: usize, limit: usize) -> i32 {
-    let mut current: usize = 0;
-    let mut marbles = vec![0];
-    let mut scores: Vec<i32> = vec![0; players];
+fn part_a(players: usize, limit: usize) -> u64 {
+    let mut marbles = VecDeque::new();
+    let mut scores: Vec<u64> = vec![0; players];
+    marbles.push_front(0);
 
     for value in 1..limit {
 
         if value % 23 == 0 {
-            let remove_index = get_index(current as i32 - 7, marbles.len());
-            let removed = marbles.remove(remove_index);
-            scores[value % players] += removed + value as i32;
-            current = remove_index;
+            marbles.rotate_right(get_index(7, marbles.len()));
+            let removed = marbles.remove(0).unwrap();
+            scores[value % players] += removed + value as u64;
         } else {
-            let new_index = get_index(current as i32 + 2, marbles.len());
-            marbles.insert(new_index, value as i32);
-            current = new_index;
+            marbles.insert(get_index(2, marbles.len()), value as u64);
+            marbles.rotate_left(2);
         }
 
-        // println!("{}:\t{:?}", (player % players) + 1, marbles);
-
-        if value % 10000 == 0 { println!("{}", value); }
+        // println!("{}:\t{:?}", (value % players) + 1, marbles);
     }
 
     let mut highest_score = 0;
-    println!("{:?}", scores);
     for i in scores {
         if highest_score < i { highest_score = i; }
     }
