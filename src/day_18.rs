d9c501eee17d8e989b0ce8873a8b2046ad20986e
MeesCode <m.brinkhuis@student.tudelft.nl>
2021-11-14 12:30:30 +0100

    cleanup

--- src/day_18.rs
+++ src/day_18.rs
@@ -21,35 +21,39 @@ pub fn run(){
     println!("Part B result: {}", b);
 }
 
+fn minute(grid: &mut Vec<Vec<char>>) {
+
+    let grid_old = grid.clone();
+
+    for y in 1..grid.len()-1 {
+        for x in 1..grid[0].len()-1 {
+            
+            let mut trees = 0;
+            let mut lumberyards = 0;
+
+            for dy in y-1..y+2 {
+                for dx in x-1..x+2 {
+                    if dx == x && dy == y { continue; } 
+                    if grid_old[dy][dx] == '|' { trees += 1; }
+                    if grid_old[dy][dx] == '#' { lumberyards += 1; }
+                }
+            }
+
+            if grid_old[y][x] == '.' && trees >= 3 { grid[y][x] = '|'; }
+            if grid_old[y][x] == '|' && lumberyards >= 3 { grid[y][x] = '#'; }
+            if grid_old[y][x] == '#' && (lumberyards < 1 || trees < 1) { grid[y][x] = '.'; }
+
+        }
+    }
+
+}
+
 fn part_a(grid: &Vec<Vec<char>>) -> i32 {
 
     let mut grid = grid.clone();
-    let mut grid_mod = grid.clone();
 
     for _step in 0..10 {
-
-        for y in 1..grid.len()-1 {
-            for x in 1..grid[0].len()-1 {
-                
-                let mut trees = 0;
-                let mut lumberyards = 0;
-
-                for dy in y-1..y+2 {
-                    for dx in x-1..x+2 {
-                        if dx == x && dy == y { continue; } 
-                        if grid[dy][dx] == '|' { trees += 1; }
-                        if grid[dy][dx] == '#' { lumberyards += 1; }
-                    }
-                }
-
-                if grid[y][x] == '.' && trees >= 3 { grid_mod[y][x] = '|'; }
-                if grid[y][x] == '|' && lumberyards >= 3 { grid_mod[y][x] = '#'; }
-                if grid[y][x] == '#' && (lumberyards < 1 || trees < 1) { grid_mod[y][x] = '.'; }
-
-            }
-        }
-
-        grid = grid_mod.clone();
+        minute(&mut grid);
     }
 
     let mut trees = 0;
@@ -67,14 +71,12 @@ fn part_a(grid: &Vec<Vec<char>>) -> i32 {
 fn part_b(grid: &Vec<Vec<char>>) -> i32 {
 
     let mut grid = grid.clone();
-    let mut grid_mod = grid.clone();
-
     let mut amounts: Vec<(i32, i32)> = Vec::new();
-
     const ITERATIONS: i32 = 1000000000;
 
     for _step in 0..100000000 {
 
+        // assume that the grid is settled after 1000 steps
         if _step >= 1000 {
             let mut trees = 0;
             let mut lumberyards = 0;
@@ -85,6 +87,7 @@ fn part_b(grid: &Vec<Vec<char>>) -> i32 {
                 }
             }
 
+            // found repetition, return correct amount
             if amounts.contains(&(trees, lumberyards)){
                 let todo = ITERATIONS - _step;
                 let index = todo as usize % amounts.len();
@@ -95,28 +98,7 @@ fn part_b(grid: &Vec<Vec<char>>) -> i32 {
 
         }
 
-        for y in 1..grid.len()-1 {
-            for x in 1..grid[0].len()-1 {
-                
-                let mut trees = 0;
-                let mut lumberyards = 0;
-
-                for dy in y-1..y+2 {
-                    for dx in x-1..x+2 {
-                        if dx == x && dy == y { continue; } 
-                        if grid[dy][dx] == '|' { trees += 1; }
-                        if grid[dy][dx] == '#' { lumberyards += 1; }
-                    }
-                }
-
-                if grid[y][x] == '.' && trees >= 3 { grid_mod[y][x] = '|'; }
-                if grid[y][x] == '|' && lumberyards >= 3 { grid_mod[y][x] = '#'; }
-                if grid[y][x] == '#' && (lumberyards < 1 || trees < 1) { grid_mod[y][x] = '.'; }
-
-            }
-        }
-
-        grid = grid_mod.clone();
+        minute(&mut grid);
 
     }
 
